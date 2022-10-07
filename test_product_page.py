68be4e67cd33316582a7a5857f613e5b287e9b65
Writer42 <writer42@mail.ru>
2022-10-07 14:11:46 +0600

    product page test rename

--- test_product_page.py
+++ test_product_page.py
@@ -21,3 +21,16 @@ def test_guest_can_add_product_to_basket(browser,link):
     page.add_to_cart()
     page.solve_quiz_and_get_code()
     page.main_checks()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
