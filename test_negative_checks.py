be45daeb94e29cc0ca5e59dfecd466ed77357dd8
Writer42 <writer42@mail.ru>
2022-10-07 15:16:58 +0600

    add xfail marks

--- test_negative_checks.py
+++ test_negative_checks.py
@@ -1,9 +1,12 @@
+import pytest
+
 from pages.product_page import ProductPage
 
 
 link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-girl-who-played-with-non-fire_203/"
 
 
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -17,6 +20,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
